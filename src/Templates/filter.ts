9eed1c56808fb313adb6686dca3e8629707a5771
Florian <sicilia.florian02@gmail.com>
2023-03-17 12:56:42 +0100

    retouche css

--- src/Templates/filter.ts
+++ src/Templates/filter.ts
@@ -4,6 +4,12 @@ type Keywords = {
     [key: string]: HTMLLIElement[]
 };
 
+enum Colors {
+    ingredients = "#3282f7",
+    appliances = "#68d9a4",
+    ustensiles = "#ed6454"
+}
+
 export class FilterTemplate {
     public keywords: Keyword;
 
@@ -45,12 +51,15 @@ export class FilterTemplate {
         list.forEach((l) => l.remove());
 
         div.forEach((el) => {
-            const label: string = el.dataset.label || "";
+            const label = el.dataset.label || "";
 
             for (const key in keywords) {
                 if (label === key) {
                     const ul = document.createElement("ul");
                     ul.classList.add("keywords-list");
+                    const color = label as keyof typeof Colors;
+                    ul.style.setProperty("background-color", Colors[color]);
+
                     ul.append(...keywords[label]);
                     el.append(ul);
                 }
@@ -62,13 +71,16 @@ export class FilterTemplate {
         const btn = e.target as HTMLButtonElement;
         btn.classList.add("filter-btn--hidden");
         btn.nextElementSibling?.classList.add("keywords-active");
+        btn.parentElement?.classList.add("filter-active");
     }
 
     close() {
         const keywordsList = document.querySelector(".keywords-active") as HTMLDivElement;
         const btn = document.querySelector(".filter-btn--hidden") as HTMLButtonElement;
+        const parent = document.querySelector(".filter-active") as HTMLDivElement;
 
         btn?.classList.remove("filter-btn--hidden");
         keywordsList?.classList.remove("keywords-active");
+        parent?.classList.remove("filter-active");
     }
 }
